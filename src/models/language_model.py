17e1b9707a4e753c7f5644758414d8a601e7bf39
Marcos Adriano <marcosadriano.ti@gmail.com>
2023-09-05 17:05:26 -0300

    Req 02

--- src/models/language_model.py
+++ src/models/language_model.py
@@ -11,7 +11,10 @@ class LanguageModel(AbstractModel):
 
     # Req. 2
     def to_dict(self):
-        raise NotImplementedError
+        return {
+            'name': self.data['name'],
+            'acronym': self.data['acronym']
+        }
 
     # Req. 3
     @classmethod
