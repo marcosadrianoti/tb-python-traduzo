b202bc44fb52ecda7e394a3c9eb859ce3ece4db6
Marcos Adriano <marcosadriano.ti@gmail.com>
2023-09-06 15:54:22 -0300

    Req 07

--- tests/models/history/test_history_model.py
+++ tests/models/history/test_history_model.py
@@ -1,7 +1,27 @@
-# import json
-# from src.models.history_model import HistoryModel
+import json
+from src.models.history_model import HistoryModel
+# from tests.models.history.conftest import prepare_base
 
 
 # Req. 7
 def test_request_history():
-    raise NotImplementedError
+    response = json.loads(
+        HistoryModel.list_as_json(),
+        # https://docs.python.org/3/library/json.html
+        object_hook=lambda dic: {
+            key: value for key, value in dic.items() if key != "_id"
+        },
+    )
+    expected = [
+        {
+            "text_to_translate": "Hello, I like videogame",
+            "translate_from": "en",
+            "translate_to": "pt",
+        },
+        {
+            "text_to_translate": "Do you love music?",
+            "translate_from": "en",
+            "translate_to": "pt",
+        },
+    ]
+    assert response == expected
