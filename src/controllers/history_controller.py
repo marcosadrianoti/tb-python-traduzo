2c8064607565fc57ee0ab9c2dbad659817dbe424
Marcos Adriano <marcosadriano.ti@gmail.com>
2023-09-07 12:49:22 -0300

    Included HTTP status 200 in history_controller

--- src/controllers/history_controller.py
+++ src/controllers/history_controller.py
@@ -6,4 +6,4 @@ history_controller = Blueprint("history_controller", __name__)
 
 @history_controller.route("/", methods=["GET"])
 def get_history():
-    return jsonify(HistoryModel.list_as_json())
+    return jsonify(HistoryModel.list_as_json()), 200
